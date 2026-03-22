3126310f4a64f7b492b4642c38182d7572c58e81
VinujiThasara <thasaravinuji26@gmail.com>
2026-03-23 04:32:30 +0530

    update
    update schedule route

--- backend/src/routes/scheduleRoutes.js
+++ backend/src/routes/scheduleRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const scheduleController = require('../controllers/scheduleController');
 
 // Get personalized schedule for the user
-router.get('/my-schedule', scheduleController.getMySchedule);
+
 
 // Get today/tomorrow notifications
 router.get('/notifications', scheduleController.getNotifications);
