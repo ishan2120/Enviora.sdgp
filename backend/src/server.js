4f285f9d2b55f9f1d4e5e10ed360dee92f21caf8
Ishan Sandeepa <ishansandeepa1020@gmail.com>
2026-03-23 05:25:55 +0530

    make the port 5000
    api service

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
 const app = express();
-const PORT = process.env.PORT || 4007;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
