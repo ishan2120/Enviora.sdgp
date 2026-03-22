1036d0be1050003afc65b5081776a6260aa9ee72
Yomali Madanayake <yomalimadanayake@gmail.com>
2026-03-23 05:21:26 +0530

    search bar

--- lib/pages/segregation_guide.dart
+++ lib/pages/segregation_guide.dart
@@ -249,7 +249,7 @@ class _SegregationGuideScreenState extends State<SegregationGuideScreen> {
     );
   }
 
-  // ── Search Bar ─────────────────────────────────────────────
+  // ── Search Bar ────────
 
   Widget _buildSearchBar() {
     return Container(
