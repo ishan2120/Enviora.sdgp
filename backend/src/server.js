e7c349476f7c4fddd3d5e6694ec1055b16cad122
Ishan Sandeepa <ishansandeepa1020@gmail.com>
2026-03-23 05:24:42 +0530

    Change the server port
    server port

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4007;
 
 app.use(cors());
 app.use(bodyParser.json());
